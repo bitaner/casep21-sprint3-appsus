1bd300696ba7cf571a9016e7bae97c8c49f8355e
Aner Biton <aner.biton@gmail.com>
2021-11-11 10:01:16 +0200

    changed imports

--- apps/keep/keep-app.cmp.js
+++ apps/keep/keep-app.cmp.js
@@ -1,9 +1,8 @@
-import { noteService } from '/apps/keep/services/note-service.js'
-import noteFilter from '/apps/keep/cmps/note-filter.cmp.js'
-import noteAdd from '/apps/keep/cmps/note-add.cmp.js'
-import noteList from '/apps/keep/cmps/note-list.cmp.js'
-import { eventBus } from '/services/event-bus-service.js'
-
+import { noteService } from './services/note-service.js'
+import noteFilter from './cmps/note-filter.cmp.js'
+import noteAdd from './cmps/note-add.cmp.js'
+import noteList from './cmps/note-list.cmp.js'
+import { eventBus } from '../../services/event-bus-service.js'
 
 export default {
     components: {
