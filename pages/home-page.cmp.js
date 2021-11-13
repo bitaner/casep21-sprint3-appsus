e30d95a5d540e525d12b9699f9eb68f433c4b427
Eden <edenmatuk@gmail.com>
2021-11-13 18:01:00 +0200

    18:00

--- pages/home-page.cmp.js
+++ pages/home-page.cmp.js
@@ -1,7 +1,8 @@
 export default {
     template: `
         <section class="home-page app-main">
-            <h5>welcome to your <span>amazing</span> appsus!</h5>
+            <h5>welcome to your amazing</h5>
+            <p><i class="far fa-lightbulb"></i>appsus!</p>
             
         </section>
     `
