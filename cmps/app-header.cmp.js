bc6611cde51d1c72923d97907662247a18af44b5
Eden <edenmatuk@gmail.com>
2021-11-13 20:14:10 +0200

    20:14

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -9,8 +9,13 @@ export default {
                 <router-link class="link" to="/book">books</router-link> |
                 <router-link class="link" to="/mail">Mail</router-link> |
                 <router-link class="link" to="/keep">Keep</router-link> |
-                <router-link class="link" to="/about">About</router-link> |
+                <router-link class="link" to="/about">About</router-link> 
             </nav>
         </header>
     `,
+    date(){
+        return {
+            show: false,
+        }
+    },
 }
